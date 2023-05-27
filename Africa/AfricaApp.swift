9f0579028332ad80d7b2018d6080a2463e44c22e
AleScorrano <alescorrano94@gmail.com>
2023-05-27 12:38:49 +0200

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Alessandro Sc on 27/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
